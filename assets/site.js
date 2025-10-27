9e5efc59a257e333250f826b0c9900323c96257e
zhamadeh <zeid.hamadeh@hotmail.com>
2025-10-27 12:25:41 -0700

    add vibrations trailer

--- assets/site.js
+++ assets/site.js
@@ -1,12 +1,12 @@
+
 const brand = document.querySelector('.brand');
 const title = document.getElementById('site-title');
-
-if (brand && title) {
-  const io = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) brand.classList.remove('show');
-      else brand.classList.add('show');
+if (brand && title && 'IntersectionObserver' in window){
+  const io = new IntersectionObserver((entries)=>{
+    entries.forEach(e=>{
+      if(e.isIntersecting){ brand.classList.remove('show'); }
+      else { brand.classList.add('show'); }
     });
-  });
+  }, {threshold: 0.01});
   io.observe(title);
 }
